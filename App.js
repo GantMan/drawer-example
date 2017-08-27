4354a93e2726bddf6e19863d0b9ba394b19331a9
Gant Laborde <GantMan@gmail.com>
2017-08-27 14:16:02 -0500

    add helper info

--- App.js
+++ App.js
@@ -2,7 +2,19 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Provider } from 'react-redux'
 import createStore from './Redux'
-import AppNavigation from './Navigation/AppNavigation'
+/*
+* Both of the following files work for react-navigation
+* Routes will always be added and supported by modifying
+* the AppNavigation file.  Special redux actions/reducers
+* will be handled in Redux Navigation
+*   // use this to use react-navigation no redux
+*   import AppNavigation from './Navigation/AppNavigation'
+*
+*   // use this to use react-navigation with redux
+*   import ReduxNavigation from './Navigation/ReduxNavigation'
+*/
+
+// We're going to use navigation with redux
 import ReduxNavigation from './Navigation/ReduxNavigation'
 
 // create our store
